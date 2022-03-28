2caa41fc384cc2538986cdc2f2dece23ccaf55b1
didges <didges73@gmail.com>
2022-03-28 19:08:54 +0300

    markup_wrapper

---
+++ markup/markup_wrapper.py
@@ -0,0 +1,14 @@
+import cv2
+import gym
+import numpy as np
+from gym import spaces
+import vis
+
+class MarkupWrapper(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(MarkupWrapper, self).__init__(env)
+
+    def observation(self, observation):
+        lane_lines = vis.detect_lane(observation)
+        lane_lines_image = vis.display_lines(observation, lane_lines)
+        return lane_lines_image
