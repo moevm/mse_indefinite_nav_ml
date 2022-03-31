0527371aa3cc68d32a469db311d6a743662e1881
Кортев Юрий Вячеславович <iu.kortev@stc-spb.ru>
2022-03-31 22:25:23 +0300

    fixed README.md, Dockerfile

--- tools/train.sh
+++ tools/train.sh
@@ -3,4 +3,4 @@ Xvfb :1 -screen 1 1024x768x24 -ac +extension GLX +render -noreset &> xvfb.log &
 export DISPLAY=:1
 
 
-python3 ./tools/train.py --conf_path ./configs/conf.py
+python3 train.py --conf_path ./configs/conf.py
