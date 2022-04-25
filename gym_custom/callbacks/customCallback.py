8c0ad2956e3bbf168da80f77b4497499b44bd55d
Роман Павлов <55055449+Crunchy0@users.noreply.github.com>
2022-04-25 18:59:58 +0300

    Исправление CustomCallback

--- gym_custom/callbacks/customCallback.py
+++ gym_custom/callbacks/customCallback.py
@@ -10,33 +10,43 @@ from PIL import Image
 from ray.rllib.agents import callbacks
 from pprint import pprint
 import torch
+import cv2
+import gym
+#from tensorboardX import SummaryWriter
+
 
 class CustomCallback(DefaultCallbacks):
-    #def __init__(self, period):
-    #    self._stepcount = 0
-    #    self._period = period
-    
-    def on_episode_end(self,
-                       *,
-                       worker: "RolloutWorker",
-                       base_env: BaseEnv,
-                       policies: Dict[PolicyID, Policy],
-                       episode: Episode,
-                       env_index: Optional[int] = None,
-                       **kwargs) -> None:
-
-        env = worker.env_creator(copy.deepcopy(worker.env_context))
-        policy = policies['default_policy']
-        env.reset()
-        obs, reward, done, info = env.step((0, 0))
-        for i in range(10):
-            while not done:
-                input_dict = {"obs": [obs]}
-                action = policy.compute_actions_from_input_dict(input_dict)
-                action = action[2]['action_dist_inputs']
-                action = (action[0][0], action[0][1])
-                print(action)
-                obs, reward, done, info = env.step(action)
-                img = Image.fromarray(np.uint8(env.render())).convert("RGB")
-                img.save(f"{worker.io_context.log_dir}/test.png")
-                exit(1)
+    def __init__(self):
+        super().__init__()
+        self.iteration = 0
+        self.log_rate = 3
+        #self.writer = SummaryWriter()
+                
+    def on_train_result(self, *, trainer: "Trainer", result: dict, **kwargs) -> None:
+        if self.iteration % self.log_rate == 0: 
+            env = trainer.env_creator(trainer.config['env_config'])
+            #env = gym.wrappers.Monitor(env, f'{trainer.logdir}/project{self.iteration}')
+            policy = trainer.get_policy()
+            video = []
+            self.iteration += 1
+            for i in range(5):
+                done = False
+                obs = env.reset()
+                while not done:
+                    input_dict = {"obs": [obs]}
+                    action = policy.compute_actions_from_input_dict(input_dict)
+                    action = action[2]['action_dist_inputs']
+                    action = (action[0][0], action[0][1])
+                    obs, reward, done, info = env.step(action)
+                    img = Image.fromarray(np.uint8(env.render())).convert("RGB")
+                    img = img.rotate(180, expand=True)
+                    video.append(np.array(img.copy()))
+            video = np.array(video)
+            size, height, width, channels = video.shape
+            
+            video = torch.Tensor([video])
+            video = video.reshape((size, channels, 1, height, width))
+            result['video'] = video
+            #self.writer.add_video(f'{trainer.logdir}/video{self.iteration}', video, fps=30)
+            
+        self.iteration += 1
